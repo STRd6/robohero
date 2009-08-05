61a1396290ac5739a9c19803a5c4fc7dfd58070b
dasblodobst <dasblodobst@5eb4d55a-73dc-11de-aabe-85d6403c52bf>
2009-08-05 00:46:34 +0000

    ??

--- trunk/db/schema.rb
+++ trunk/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090729015624) do
+ActiveRecord::Schema.define(:version => 20090805003529) do
 
   create_table "accounts", :force => true do |t|
     t.string   "nickname"
@@ -53,6 +53,11 @@ ActiveRecord::Schema.define(:version => 20090729015624) do
   create_table "decks", :force => true do |t|
     t.string   "name"
     t.integer  "size"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "games", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
