622e273eb34402bcd0d59badf6229cae594c189a
yahivin <yahivin@5eb4d55a-73dc-11de-aabe-85d6403c52bf>
2009-08-05 03:02:32 +0000

    Added all card import methods.

--- lib/card_importer.rb
+++ lib/card_importer.rb
@@ -22,8 +22,102 @@ class CardImporter
           :ready_time => weapon_hash[:ready],
           :rules_text => rules_text
         )
-        puts card
       end
     end
+  end
+  
+  def self.import_utilities
+    utility_data = FasterCSV.table(DATA_DIR + "utilities.csv")
+
+    utility_data.each do |datum|
+      hash = datum.to_hash
+
+      card_name = hash[:card_name]
+      unless card_name.blank?
+        card = Card.find_or_create_by_name(card_name)
+
+        rules_text = ""
+        unless hash[:rules_text] == 0
+          rules_text += hash[:rules_text]
+        end
+
+        flavor_text = ""
+        unless hash[:flavor_text] == 0
+          flavor_text = hash[:flavor_text]
+        end
+
+        card.update_attributes!(
+          :dollar_cost => hash[:deploy_cost],
+          :ready_time => hash[:ready],
+          :rules_text => rules_text,
+          :flavor_text => flavor_text || ""
+        )
+      end
+    end
+  end
+
+  def self.import_upgrades
+    upgrade_data = FasterCSV.table(DATA_DIR + "upgrades.csv")
+
+    upgrade_data.each do |datum|
+      hash = datum.to_hash
+
+      card_name = hash[:card_name]
+      unless card_name.blank?
+        card = Card.find_or_create_by_name(card_name)
+
+        rules_text = ""
+        unless hash[:rules_text] == 0
+          rules_text += (hash[:rules_text] || "")
+        end
+
+        flavor_text = ""
+        unless hash[:flavor_text] == 0
+          flavor_text = hash[:flavor_text]
+        end
+
+        card.update_attributes!(
+          :dollar_cost => hash[:deploy_cost],
+          :rules_text => rules_text,
+          :flavor_text => flavor_text || ""
+        )
+      end
+    end
+  end
+
+  def self.import_instants
+    instant_data = FasterCSV.table(DATA_DIR + "instants.csv")
+
+    instant_data.each do |datum|
+      hash = datum.to_hash
+
+      card_name = hash[:card_name]
+      unless card_name.blank?
+        card = Card.find_or_create_by_name(card_name)
+
+        rules_text = ""
+        unless hash[:rules_text] == 0
+          rules_text += hash[:rules_text]
+        end
+
+        flavor_text = ""
+        unless hash[:flavor_text] == 0
+          flavor_text = hash[:flavor_text]
+        end
+
+        card.update_attributes!(
+          :energy_cost => hash[:energy_cost],
+          :rules_text => rules_text,
+          :flavor_text => flavor_text || ""
+        )
+      end
+    end
+  end
+
+  def self.import_all
+    import_weapons
+    import_utilities
+    import_upgrades
+    import_instants
   end
 end
