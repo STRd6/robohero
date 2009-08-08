feb5251cf965c271bd04e7404626e1346e8cb5a3
xboxdude08 <xboxdude08@5eb4d55a-73dc-11de-aabe-85d6403c52bf>
2009-08-08 05:22:55 +0000

    more javascript in the view, and the start of ajax requests

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,14 +1,48 @@
 $(function(){
-    $('.card').draggable({snap:true});
+    var url = 'http://localhost:3000/';
+    var data = 'zomg test';
+    function callback() {alert("wow it worked");}
+    $('.card').draggable({revert:'invalid'});
+    $('.card').click(function(){
+      $('.detail').html($(this).html());
+      
+    });
     $('.deck').click(function(){addCardToHand()});
+    $('.counterCreator').click(function(){createCounter()});
     $('.discard').droppable({
       drop: function(event, ui) {
-        console.log(ui);
-        console.log($(ui));
+        var id = ui.draggable.context.children[3].innerHTML;
         ui.draggable.remove();
+        $.post(url + 'cards/' + id + '/discard', data, callback());
+        
     }})
+    $('.slot').droppable({
+        drop: function(event, ui) {
+          var id = ui.draggable.context.children[3].innerHTML;
+          $.post(url + 'cards/' + id + '/played', data, callback());
+          ui.draggable.appendTo($(event.target));
+          ui.draggable.css({top:-30,left:10});
+      }})
+    $('.hand').droppable({
+        drop: function(event, ui) {
+          ui.draggable.appendTo($(event.target));
+          ui.draggable.css({top:-10});
+        }
+    })
+    $('.card').droppable({
+        accept: '.counter',
+        hoverClass: 'ui-state-active',
+        drop: function(event, ui) {
+          ui.draggable.appendTo($(event.target));
+          ui.draggable.css({top:-10,left:0});
+          
+        }
+    })
 });
 
 function addCardToHand(){
-    $('<div class="card" />').draggable({snap:true}).appendTo('.hand');
+//    $('#allCards').children()[0].removeClass('hidden');
+}
+function createCounter(){
+  $('<div class="counter" />').draggable({snap:false, revert:'invalid'}).appendTo('.counterDropZone');
 }
