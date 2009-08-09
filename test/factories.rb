fced1ec713ff9df3a792f0f31ca84ba0457ccbf8
yahivin <yahivin@5eb4d55a-73dc-11de-aabe-85d6403c52bf>
2009-08-09 19:28:22 +0000

    Added versioning to deck lists.

--- test/factories.rb
+++ test/factories.rb
@@ -8,3 +8,16 @@ end
 Factory.define :game do |game|
   
 end
+
+Factory.define :account do |account|
+  
+end
+
+Factory.sequence(:deck_list_name) do |n|
+  "DECK_#{n}"
+end
+
+Factory.define :deck_list do |deck_list|
+  deck_list.name { Factory.next(:deck_list_name) }
+  deck_list.association :account
+end
