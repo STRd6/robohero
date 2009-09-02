b53f64c07009e552b9a7ea7d40b326cce1a26c72
yahivin <yahivin@5eb4d55a-73dc-11de-aabe-85d6403c52bf>
2009-09-02 04:54:05 +0000

    Adding some FBML.

--- lib/facebook_controller.rb
+++ lib/facebook_controller.rb
@@ -2,6 +2,7 @@ module FacebookController
   def self.included(controller)
     controller.class_eval do
       index.wants.fbml {}
+      new_action.wants.fbml {}
       show.wants.fbml {}
     end
   end
