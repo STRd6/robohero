1a84e29407220ff6f8c4a7209f8dc266ca43b6e3
yahivin <yahivin@5eb4d55a-73dc-11de-aabe-85d6403c52bf>
2009-09-04 05:16:01 +0000

    Testing some FBML

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,35 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  include Facebooker::Rails::Controller
+
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 
+  #TODO: Don't make FB only, eventually
+  ensure_authenticated_to_facebook
   before_filter :set_up_user
 
+  helper_method :fb_uid
+
   private
+
   def set_up_user
-    
+    @current_account = FacebookLogin.establish_account(facebook_session.user.uid)
+  end
+
+  def establish_credentials
+    #TODO:
+    # Check cookies
+    # Check FB
+    # Twitter?
+    # Other Auth mechanisms?
+  end
+
+  def fb_uid
+    facebook_session.user.uid if facebook_session
   end
 end
