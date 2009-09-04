491ee743f1c6dc898d5983cf9c1412bba010c6ed
yahivin <yahivin@5eb4d55a-73dc-11de-aabe-85d6403c52bf>
2009-09-04 06:52:27 +0000

    Finally... testing.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,17 +1,19 @@
 class MainController < ApplicationController
-  def link
-    link_id = Link.find_by_enc_id(params[:token]).id
+  def process_link
+    if(link = Link.find_by_token(params[:token]))
+      LinkEvent.find_or_create_by_link_id_and_session_id_and_action(
+        link.id,
+        session.session_id,
+        :clicked
+      )
 
-    LinkEvent.find_or_create_by_link_id_and_session_id_and_state(
-      link_id,
-      session.session_id,
-      :clicked
-    )
+      session[:link_id] = link.id
 
-    session[:link_id] = link_id
-
-    if link.target
-      redirect_to link.target
+      if link.target
+        redirect_to link.target
+      else
+        redirect_to '/'
+      end
     else
       redirect_to '/'
     end
