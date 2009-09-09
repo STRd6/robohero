e9d73c61a78958a5171305cdad3e2c1ad28f11ff
yahivin <yahivin@5eb4d55a-73dc-11de-aabe-85d6403c52bf>
2009-09-09 06:09:51 +0000

    Added rudimentary deck list creation.

--- trunk/config/routes.rb
+++ trunk/config/routes.rb
@@ -44,7 +44,7 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
-  map.resources :games, :cards
+  map.resources :cards, :deck_lists, :games
 
   map.link '/q/:token', :controller => 'main', :action => 'process_link'
   
